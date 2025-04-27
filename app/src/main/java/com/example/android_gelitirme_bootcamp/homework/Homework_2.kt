db5cc634e403db211d9e54cbea179040bc892a6a
John Doe <canliyaren0@gmail.com>
2025-04-27 20:44:08 +0300

    Homework güncellendi

--- app/src/main/java/com/example/android_gelitirme_bootcamp/homework/Homework_2.kt
+++ app/src/main/java/com/example/android_gelitirme_bootcamp/homework/Homework_2.kt
@@ -2,7 +2,7 @@ package com.example.android_gelitirme_bootcamp.homework
 
 
 
-fun main(){
+fun main() {
 
     // 1-Parametre olarak girilen dereceyi fahrenheit'a dönüştüren fonksiyon
 
@@ -11,17 +11,30 @@ fun main(){
     println("$derece derece = $fahrenheit Fahrenheit")
 
 
-    //2-Kenarları parametre olarak girilen ve dikdörtgenin çevresini hesaplayan metod
+    //2-Kenarları girilen ve dikdörtgenin çevresini hesaplayan metod
     val k1 = 18
     val k2 = 43
 
     val sonuc = (2 * k1) + (2 * k2)
     println("Kenarları $k1 ve $k2 olan dikdörtgenin çevresi: $sonuc")
 
+    //3-Verilen sayının faktöriyel hesabı
+    val sayi = 5
+    val faktoriyel = faktoriyelHesapla(sayi)
+    println("$sayi sayısının faktöriyeli = $faktoriyel")
+
 }
 
 fun donusum(derece: Double) : Double{
     return((derece * 1.8) + 32)
+}
+
+fun faktoriyelHesapla(sayi: Int): Int {
+    var sonuc2 = 1
+    for (i in 1..sayi) {
+        sonuc2 *= i
+    }
+    return sonuc2
 }
 
 
