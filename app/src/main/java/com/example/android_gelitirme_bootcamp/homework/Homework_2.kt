49c73930991731cca404b6a966287df789fcbf8d
John Doe <canliyaren0@gmail.com>
2025-04-27 19:09:02 +0300

    Homework güncellendi

--- app/src/main/java/com/example/android_gelitirme_bootcamp/homework/Homework_2.kt
+++ app/src/main/java/com/example/android_gelitirme_bootcamp/homework/Homework_2.kt
@@ -1,2 +1,11 @@
 package com.example.android_gelitirme_bootcamp.homework
 
+fun main(){
+    val derece = 35.0
+    val fahrenheit = donusum(derece)
+    println("$derece derece = $fahrenheit Fahrenheit")
+}
+
+fun donusum(derece: Double) : Double{
+    return((derece * 1.8) + 32)
+}
